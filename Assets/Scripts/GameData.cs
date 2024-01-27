6ccacebeb0ba413b85cceed7caeec4ffabb9eff9
NightWalkerX <NightWalkerXin@outlook.com>
2024-01-27 13:11:23 +0800

    gamedata

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : Singleton<GameData>
+{
+    public int Money = 0;
+    public int Supplies = 0;
+    public int Science_Point = 0;
+    public int Happiness = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
