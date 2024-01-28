e7157c187868f90fb9820cfb0191d87cd7640333
NightWalkerX <NightWalkerXin@outlook.com>
2024-01-28 14:33:46 +0800

    资金上限

--- Assets/Scripts/UI/Money_UI.cs
+++ Assets/Scripts/UI/Money_UI.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Money_UI : MonoBehaviour
 {
     public Image money_Image;
+    public TMP_Text money_Limit_Text;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +25,8 @@ public class Money_UI : MonoBehaviour
         money_Image.fillAmount = (float)(GameData.GetInstance().All_Money_Current_Turn-GameData.GetInstance().Money_Spend_Current_Turn) / (float)GameData.GetInstance().All_Money_Current_Turn;
         //Debug.Log("本回合消耗的资金： " + GameData.GetInstance().Money_Spend_Current_Turn);
         //Debug.Log("本回全部资金： " + GameData.GetInstance().All_Money_Current_Turn);
+
+        money_Limit_Text.text = "资金上限：" + GameData.GetInstance().money_Limit[GameData.GetInstance().finance_Level];
     }
 
 }
